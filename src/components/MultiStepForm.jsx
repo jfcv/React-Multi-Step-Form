609ad10854d6f07036464c3bb8cfb6ac7c900071
velveet <velveet@pm.me>
2020-08-18 15:14:12 -0500

    useForm() hook added for handling the Form data. useStep() hook added for handling the Form steps. steps Form components created: Names, Address, Contact, Review & Submit.

--- src/components/MultiStepForm.jsx
+++ src/components/MultiStepForm.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useForm, useStep } from 'react-hooks-helper'
+
+import Names from './stepForm/Names'
+import Address from './stepForm/Address'
+import Contact from './stepForm/Contact'
+import Review from './stepForm/Review'
+import Submit from './stepForm/Submit'
+
+const defaultData = {
+    firstName: '',
+    lastName: '',
+    nickName: ''
+};
+
+const steps = [
+    { id: 'names'},
+    { id: 'address'},
+    { id: 'contact'},
+    { id: 'review'},
+    { id: 'submit'},
+];
 
 function MultiStepForm() {
+    
+    const [formData, setForm] = useForm(defaultData);
+
+    const { step, navigation } = useStep({
+        steps,
+        initialStep: 0,
+    })
+
+    switch (step.id) {
+        case 'names':
+            return <Names />;
+        case 'address':
+            return <Address />;
+        case 'contact':
+            return <Contact />;
+        case 'review':
+            return <Review />;
+        case 'submit':
+            return <Submit />;
+    }
+
     return(
         <div>
-            <h1>Hi</h1>
+            <h1>Multi Step Form</h1>
         </div>
     )
 }
---
+++ src/components/stepForm/Names.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Names() {
+    return(
+        <div>
+            <h1>Names</h1>
+        </div>
+    )
+}
+
+export default Names
