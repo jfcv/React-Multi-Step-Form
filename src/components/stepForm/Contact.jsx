0d381b430fd0b64e8d768d64e98b9e7b0da6c022
velveet <velveet@pm.me>
2020-08-18 18:39:14 -0500

    'Contact' component styled.

--- src/components/stepForm/Contact.jsx
+++ src/components/stepForm/Contact.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
-function Contact() {
+function Contact({ formData, setForm, navigation }) {
+
+    let { phone, email } = formData; //destructuring
+
     return(
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <Container maxWidth='xs'>
+
+            <h3>Contact</h3>
+
+            <TextField 
+                label='Phone'
+                name='phone'
+                value={phone}
+                onChange={setForm}
+                margin='normal'
+                variant='outlined'
+                autoComplete='off'
+                fullWidth
+            />
+
+            <TextField 
+                label='Email'
+                name='email'
+                value={email}
+                onChange={setForm}
+                margin='normal'
+                variant='outlined'
+                autoComplete='off'
+                fullWidth
+            />            
+
+            <div>
+                <Button
+                    variant='contained'
+                    fullWidth
+                    color='primary'
+                    onClick={() => {navigation.next()}}
+                    style={{marginTop: '1rem'}}
+                >
+                    Next
+                </Button>
+
+                <Button
+                    variant='contained'
+                    fullWidth
+                    onClick={() => {navigation.previous()}}
+                    style={{marginTop: '1rem'}}
+                >
+                    Back
+                </Button>
+            </div>
+
+        </Container>
     )
 }
 
